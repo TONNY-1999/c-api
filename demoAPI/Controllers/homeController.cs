cb8b3f7854c00bc5b8c3bc57fef9fe5737e0c9d2
TONNY-1999 <oduortonny10@gmail.com>
2022-09-22 08:06:31 +0300

    first commit

---
+++ demoAPI/Controllers/homeController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace demoAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class homeController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("hello world!!");
+        }
+        [HttpPost]
+        public IActionResult post(object payload) 
+        {
+
+            return Ok(payload);
+                }
+    }
+}
